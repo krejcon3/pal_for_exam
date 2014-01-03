2bc59fc5584c686486de9ad0961c5b622cab1102
Ondřej Krejčíř <ondrej.krejcir@antee.cz>
2014-01-03 14:47:44 +0100

    Počáteční commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("List of classes:");
+        System.out.println(" - Combinatorics.java");
+        System.out.println(" - Graphs.java");
+        System.out.println(" - Trees.java");
+        System.out.println(" - Heaps.java");
+        System.out.println(" - Automata.java");
+    }
+}
